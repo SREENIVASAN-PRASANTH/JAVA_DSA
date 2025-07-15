2d61e4dc2fe5ae2832f64726a48531654f2c0438
Prasanth <prasanthshot@gmail.com>
2025-07-16 00:49:01 +0530

    Tried two classes for running even or odd program

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class EvenOrOdd {
+    public static String checkEvenOrOdd(int num){
+        if(num % 2 == 0){
+            return "Even";
+        }
+        else{
+            return "Odd";
+        }
+    }
+}
---
+++ UserTest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UserTest{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        System.out.println(EvenOrOdd.checkEvenOrOdd(num));
+    }
+}
